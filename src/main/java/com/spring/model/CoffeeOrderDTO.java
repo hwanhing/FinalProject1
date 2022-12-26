8eeff9e8e955e323e17fb77b5640911c3b5bc826
hwanhing <111285100+hwanhing@users.noreply.github.com>
2022-12-26 18:42:37 +0900

    커어어어밋

--- src/main/java/com/spring/model/CoffeeOrderDTO.java
+++ src/main/java/com/spring/model/CoffeeOrderDTO.java
@@ -39,25 +39,6 @@ public class CoffeeOrderDTO {
 	
 	
 	
-	
-	private String member_id;
-	private String member_pwd;
-	private String member_name;
-	private String member_birth;
-	private String member_email;
-	private String member_phone;
-	private String member_date;
-	private String member_img;
-	private String member_addr;
-	private String member_addr2;
-	private String member_addr3;
-	private String member_addr4;
-	private String member_addr5;
-	private int member_point;		// 주문금액의 5%
-	private int test_num;
-	private int member_use;
-	
-	
 	// 테이블 외 필요
 	private int cart_num;			// 장바구니 번호
 	private String beans_img;		// 상품 이미지
